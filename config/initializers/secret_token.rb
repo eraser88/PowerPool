49b71e55bcd5cd7457808bd19183eca89e67897d
Vatsal <vatsalsinghal88@gmail.com>
2012-08-13 22:58:57 +0530

    <vatsal> Creating new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powerpool::Application.config.secret_token = '3f627b5bdd0b0d76eb76d8a4f06be8a8379d640bff60535617f51287705f604c21f88608b4e88786984936dacddc8bf31a8320502bae8ca304e53db0cac53727'
