a3d6177670d8b269878f9356996a3c09829f07a1
Zabil C.M <zabil@gmail.com>
2013-07-14 15:54:23 +0530

    Added logic and test to add routes and display the added routes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,12 @@
-    'use strict';
+'use strict';
 
 /* Controllers */
 
 angular.module('myApp.controllers', []).
     controller('PoolController', ['$scope', function($scope) {
-
+	$scope.routes = [];
+	$scope.addRoute = function(source, destination){
+	    $scope.routes.push({source : source, destination: destination});
+	    $scope.source = $scope.destination = '';
+	}
     }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,8 +5,14 @@
 describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
-  it('should add source and destination to the source and destination list', inject(function() {
-    //spec body
+  it('should add source and destination to the source and destination list', inject(function($rootScope, $controller) {
+      var scope = $rootScope.$new();
+      var ctrl = $controller('PoolController', {$scope: scope});
+      scope.addRoute('start', 'end');
+      expect(scope.routes[0].source).toBe('start');
+      expect(scope.routes[0].destination).toBe('end');
+      expect(scope.source).toBe('');
+      expect(scope.destination).toBe('');
   }));
 
   it('should ....', inject(function() {
