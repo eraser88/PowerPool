494ea36e49e09e822675a13cb315497e56858465
gohanray <gohanray@gmail.com>
2013-08-21 14:26:55 +0530

    Adding a global remove route option [removing routes only belonging to a specific user will be handled later, when implementing authentication]

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,16 @@
 
 angular.module('myApp.controllers', []).
     controller('PoolController', ['$scope', function($scope) {
-	$scope.routes = [];
-	$scope.addRoute = function(source, destination){
-	    $scope.routes.push({source : source, destination: destination});
-	    $scope.source = $scope.destination = '';
-	}
+        $scope.routes = [];
+
+        $scope.addRoute = function(source, destination){
+            $scope.routes.push({source : source, destination: destination});
+            $scope.source = $scope.destination = '';
+	    };
+
+        $scope.removeRoute = function(index){
+            if(index > -1 && index < $scope.routes.length) {
+                $scope.routes.splice(index, 1);
+            }
+        };
     }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -8,11 +8,25 @@ describe('controllers', function(){
   it('should add source and destination to the source and destination list', inject(function($rootScope, $controller) {
       var scope = $rootScope.$new();
       var ctrl = $controller('PoolController', {$scope: scope});
+
       scope.addRoute('start', 'end');
+
       expect(scope.routes[0].source).toBe('start');
       expect(scope.routes[0].destination).toBe('end');
       expect(scope.source).toBe('');
       expect(scope.destination).toBe('');
+  }));
+
+  it('should remove the source and destination from the routes list', inject(function($rootScope, $controller) {
+      var scope = $rootScope.$new();
+      var ctrl = $controller('PoolController', {$scope: scope});
+      scope.routes = [{source: 'start', destination: 'end'}, {source: 'start2', destination: 'end2'}];
+
+      scope.removeRoute(0);
+
+      expect(scope.routes.length).toBe(1);
+      expect(scope.routes[0].source).toBe('start2');
+      expect(scope.routes[0].destination).toBe('end2');
   }));
 
   it('should ....', inject(function() {
